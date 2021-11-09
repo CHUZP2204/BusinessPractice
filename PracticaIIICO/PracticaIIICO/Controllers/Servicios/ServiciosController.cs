b141a9928af011cbb423db2ddd127efa68fe4fae
Jesús Saenz <jesussaenz04@gmail.com>
2021-11-08 23:03:34 -0600

    Mantenimiento Servicios

--- PracticaIIICO/PracticaIIICO/Controllers/Servicios/ServiciosController.cs
+++ PracticaIIICO/PracticaIIICO/Controllers/Servicios/ServiciosController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PracticaIIICO.BD;
 
 namespace PracticaIIICO.Controllers.Servicios
 {
     public class ServiciosController : Controller
     {
+        MotoRepuestosMakoEntities ModeloBD = new MotoRepuestosMakoEntities();
         // GET: Servicios
         public ActionResult Index()
         {
@@ -15,55 +17,119 @@ namespace PracticaIIICO.Controllers.Servicios
         }
 
         // GET: Servicios/Details/5
-        public ActionResult Details(int id)
+        public ActionResult ListaServicios()
         {
-            return View();
+            List<sp_Retorna_Servicio_Result> datosObtenidos = new List<sp_Retorna_Servicio_Result>();
+            datosObtenidos = this.ModeloBD.sp_Retorna_Servicio(null).ToList();
+
+            this.agregaTipoServicios();
+            return View(datosObtenidos);
         }
 
         // GET: Servicios/Create
-        public ActionResult Create()
+        public ActionResult NuevoSERV()
         {
+            this.agregaTipoServicios();
             return View();
         }
 
         // POST: Servicios/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult NuevoSERV(sp_Retorna_Servicio_Result collection)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+
             try
             {
-                // TODO: Add insert logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Inserta_Servicio(
+                    collection.ID_TipoServicio,
+                    collection.Nombre_Servicio,
+                    collection.Precio_Servicio
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListaServicios");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+
+            this.agregaTipoServicios();
+            return View();
         }
 
         // GET: Servicios/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult ModificaSERV(int id_Serv)
         {
-            return View();
+            sp_Retorna_Serv_ID_Result datosObtenidos = new sp_Retorna_Serv_ID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_Serv_ID(id_Serv).FirstOrDefault();
+
+            this.agregaTipoServicios();
+            return View(datosObtenidos);
         }
 
         // POST: Servicios/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult ModificaSERV(sp_Retorna_Serv_ID_Result collection)
         {
+
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+
             try
             {
-                // TODO: Add update logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Modifica_Servicio(
+                    collection.ID_Servicio,
+                    collection.ID_TipoServicio,
+                    collection.Nombre_Servicio,
+                    collection.Precio_Servicio
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListaServicios");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Modificado";
+                }
+                else
+                {
+                    resultado += "No se pudo Modificar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+
+            this.agregaTipoServicios();
+            return View();
         }
 
+        //
+        public ActionResult MostrarSERV(int id_Serv)
+        {
+            sp_Retorna_Serv_ID_Result datosObtenidos = new sp_Retorna_Serv_ID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_Serv_ID(id_Serv).FirstOrDefault();
+
+            this.agregaTipoServicios();
+            return View(datosObtenidos);
+        }
         // GET: Servicios/Delete/5
         public ActionResult Delete(int id)
         {
@@ -84,6 +150,11 @@ namespace PracticaIIICO.Controllers.Servicios
             {
                 return View();
             }
+        }
+
+        void agregaTipoServicios()
+        {
+            this.ViewBag.ListaTipoServicios = this.ModeloBD.sp_Retorna_Tipo_Servicio(null,null).ToList();
         }
     }
 }
--- PracticaIIICO/PracticaIIICO/Controllers/Servicios/TipoServiciosController.cs
+++ PracticaIIICO/PracticaIIICO/Controllers/Servicios/TipoServiciosController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PracticaIIICO.BD;
 
 namespace PracticaIIICO.Controllers.TipoServicios
 {
     public class TipoServiciosController : Controller
     {
+        MotoRepuestosMakoEntities ModeloBD = new MotoRepuestosMakoEntities();
+        
         // GET: TipoServicios
         public ActionResult Index()
         {
@@ -15,53 +18,109 @@ namespace PracticaIIICO.Controllers.TipoServicios
         }
 
         // GET: TipoServicios/Details/5
-        public ActionResult Details(int id)
+        public ActionResult ListTypeServices()
         {
-            return View();
+            List<sp_Retorna_Tipo_Servicio_Result> datosObtenidos = new List<sp_Retorna_Tipo_Servicio_Result>();
+            datosObtenidos = this.ModeloBD.sp_Retorna_Tipo_Servicio(null, null).ToList();
+
+            return View(datosObtenidos);
         }
 
         // GET: TipoServicios/Create
-        public ActionResult Create()
+        public ActionResult NuevoTipoS()
         {
             return View();
         }
 
         // POST: TipoServicios/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult NuevoTipoS(sp_Retorna_Tipo_Servicio_Result collection)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+
             try
             {
-                // TODO: Add insert logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Inserta_TipoSERV(
+                    collection.Nombre_TipoServicio,
+                    collection.Descripcion_TipoServicio
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListTypeServices");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            
+            return View();
         }
 
         // GET: TipoServicios/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult ModificaTipoS(int id_TipoS)
         {
-            return View();
+            sp_Retorna_Tipo_SERV_ID_Result datosObtenidos = new sp_Retorna_Tipo_SERV_ID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_Tipo_SERV_ID(id_TipoS).FirstOrDefault();
+
+            return View(datosObtenidos);
         }
 
         // POST: TipoServicios/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult ModificaTipoS(sp_Retorna_Tipo_SERV_ID_Result collection)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+
             try
             {
-                // TODO: Add update logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Modifica_TipoSERV(
+                    collection.ID_TipoServicio,
+                    collection.Nombre_TipoServicio,
+                    collection.Descripcion_TipoServicio
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListTypeServices");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Modificado";
+                }
+                else
+                {
+                    resultado += "No se pudo Modificar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+
+            return View(collection);
+        }
+
+        public ActionResult MostrarTipoS(int id_TipoS)
+        {
+            sp_Retorna_Tipo_SERV_ID_Result datosObtenidos = new sp_Retorna_Tipo_SERV_ID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_Tipo_SERV_ID(id_TipoS).FirstOrDefault();
+
+            return View(datosObtenidos);
+            
         }
 
         // GET: TipoServicios/Delete/5
