cb4891f3b90d682e8fad84e8cfee1c6709d01cae
Jesús Saenz <jesussaenz04@gmail.com>
2021-11-08 20:17:47 -0600

    Mantenimiento De Usuarios

--- PracticaIIICO/PracticaIIICO/Controllers/Usuarios/TipoUsuariosController.cs
+++ PracticaIIICO/PracticaIIICO/Controllers/Usuarios/TipoUsuariosController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PracticaIIICO.BD;
 
 namespace PracticaIIICO.Controllers.TipoUsuarios
 {
     public class TipoUsuariosController : Controller
     {
+        MotoRepuestosMakoEntities ModeloBD = new MotoRepuestosMakoEntities();
+
         // GET: TipoUsuarios
         public ActionResult Index()
         {
@@ -15,53 +18,94 @@ namespace PracticaIIICO.Controllers.TipoUsuarios
         }
 
         // GET: TipoUsuarios/Details/5
-        public ActionResult Details(int id)
+        public ActionResult ListTypeUser()
         {
-            return View();
+            List<sp_Retorna_TipoUsuario_Result> modeloObtenido = new List<sp_Retorna_TipoUsuario_Result>();
+            modeloObtenido = this.ModeloBD.sp_Retorna_TipoUsuario(null).ToList();
+            return View(modeloObtenido);
         }
 
         // GET: TipoUsuarios/Create
-        public ActionResult Create()
+        public ActionResult NuevoTipoUsuario()
         {
             return View();
         }
 
         // POST: TipoUsuarios/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult NuevoTipoUsuario(sp_Retorna_TipoUsuario_ID_Result modeloVista)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
             try
             {
-                // TODO: Add insert logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Inserta_TipoUsuario(
+                    modeloVista.Nombre_TipoUsuario
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListTypeUser");
             }
-            catch
+            catch(Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            return View();
         }
 
         // GET: TipoUsuarios/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult ModificaTipoUsuario(int id_TipoU)
         {
-            return View();
+            sp_Retorna_TipoUsuario_ID_Result modeloObtenido = new sp_Retorna_TipoUsuario_ID_Result();
+
+            modeloObtenido = this.ModeloBD.sp_Retorna_TipoUsuario_ID(id_TipoU).FirstOrDefault();
+
+            return View(modeloObtenido);
         }
 
         // POST: TipoUsuarios/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult ModificaTipoUsuario(sp_Retorna_TipoUsuario_ID_Result collection)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
             try
             {
-                // TODO: Add update logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Modifica_TipoUsuario(
+                    collection.ID_TipoUsuario,
+                    collection.Nombre_TipoUsuario
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListTypeUser");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Modificado";
+                }
+                else
+                {
+                    resultado += "No se pudo Modificar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            return View();
         }
 
         // GET: TipoUsuarios/Delete/5
--- PracticaIIICO/PracticaIIICO/Controllers/Usuarios/UsuariosController.cs
+++ PracticaIIICO/PracticaIIICO/Controllers/Usuarios/UsuariosController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PracticaIIICO.BD;
 
 namespace PracticaIIICO.Controllers.Usuarios
 {
     public class UsuariosController : Controller
     {
+        MotoRepuestosMakoEntities ModeloBD = new MotoRepuestosMakoEntities();
         // GET: Usuarios
         public ActionResult Index()
         {
@@ -15,55 +17,169 @@ namespace PracticaIIICO.Controllers.Usuarios
         }
 
         // GET: Usuarios/Details/5
-        public ActionResult Details(int id)
+        public ActionResult ListaUsuarios()
         {
-            return View();
+            List<sp_Retorna_Usuario_Result> datosObtenidos = new List<sp_Retorna_Usuario_Result>();
+            datosObtenidos = this.ModeloBD.sp_Retorna_Usuario(null, null).ToList();
+            return View(datosObtenidos);
         }
 
         // GET: Usuarios/Create
-        public ActionResult Create()
+        public ActionResult NuevoUsuario()
         {
+            this.AgregTipoUsuariosViewBag();
             return View();
         }
 
         // POST: Usuarios/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult NuevoUsuario(sp_Retorna_Usuario_Result collection)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+            string parteUnaNombre = "";
+            string parteDosNombre = "";
+            string usuarioFinal = "";
+
+            string clavePorDefecto = "12345";
+
+            string estadoUsuario = "ACTIVO";
+            DateTime fechaCreado;
+            DateTime fechaUltimaVes;
+
+            ///Asignar fechas
+
+            fechaCreado = DateTime.Now;
+            fechaUltimaVes = DateTime.Now;
+
+            ///Crear Un Usuario de Logueo a partir del nombre y apellido 
+            parteUnaNombre = collection.Nombre_U.Substring(0, 3);
+            parteDosNombre = collection.Apellido1_U.Substring(0, 3);
+            ///Concatenar los datos obtnidos y unirlos
+            ///Asi Se obtnedra el Nombre Usuario Final
+            usuarioFinal = parteUnaNombre+parteDosNombre;
+
+
             try
             {
-                // TODO: Add insert logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Inserta_Usuario(
+                    collection.ID_TipoUsuario,
+                    collection.Nombre_U,
+                    collection.Apellido1_U,
+                    collection.Apellido2_U,
+                    collection.Direccion_U,
+                    collection.Correo_U,
+                    collection.Telefono_U,
+                    clavePorDefecto,
+                    usuarioFinal,
+                    estadoUsuario,//dato en string
+                    fechaCreado,
+                    fechaUltimaVes
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListaUsuarios");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            this.AgregTipoUsuariosViewBag();
+            return View();
         }
 
         // GET: Usuarios/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult ModificaUsuario(int id_User)
         {
-            return View();
+            sp_Retorna_UsuarioID_Result datosObtenidos = new sp_Retorna_UsuarioID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_UsuarioID(id_User,null,null).FirstOrDefault();
+
+            this.AgregTipoUsuariosViewBag();
+
+            return View(datosObtenidos);
         }
 
         // POST: Usuarios/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult ModificaUsuario(sp_Retorna_UsuarioID_Result collectionView)
         {
+            sp_Retorna_UsuarioID_Result datosObtenidos = new sp_Retorna_UsuarioID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_UsuarioID(collectionView.ID_Usuario, null, null).FirstOrDefault();
+            /////
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+
+            DateTime fechaUltimaVes;
+
+            ///Asignar fechas
+            fechaUltimaVes = DateTime.Now;
+
+
             try
             {
-                // TODO: Add update logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Modifica_Usuario(
+                    collectionView.ID_Usuario,
+                    collectionView.ID_TipoUsuario,
+                    collectionView.Nombre_U,
+                    collectionView.Apellido1_U,
+                    collectionView.Apellido2_U,
+                    collectionView.Direccion_U,
+                    collectionView.Correo_U,
+                    collectionView.Telefono_U,
+                    collectionView.Clave_U,
+                    collectionView.Usuario_Ingreso,
+                    datosObtenidos.Estado_U,//dato en string
+                    datosObtenidos.Fecha_Creado,
+                    fechaUltimaVes
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListaUsuarios");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Modificado";
+                }
+                else
+                {
+                    resultado += "No se pudo Modificar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+
+            this.AgregTipoUsuariosViewBag();
+
+            return View(collectionView);
         }
 
+
+        //
+        public ActionResult MostrarUsuario(int id_User)
+        {
+            sp_Retorna_UsuarioID_Result datosObtenidos = new sp_Retorna_UsuarioID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_UsuarioID(id_User, null, null).FirstOrDefault();
+
+            this.AgregTipoUsuariosViewBag();
+
+            return View(datosObtenidos);
+            
+        }
         // GET: Usuarios/Delete/5
         public ActionResult Delete(int id)
         {
@@ -84,6 +200,14 @@ namespace PracticaIIICO.Controllers.Usuarios
             {
                 return View();
             }
+        }
+
+        /// <summary>
+        /// Agregar Tipos De Roles
+        /// </summary>
+        void AgregTipoUsuariosViewBag()
+        {
+            this.ViewBag.ListaTipoUsuarios = this.ModeloBD.sp_Retorna_TipoUsuario(null).ToList();
         }
     }
 }
