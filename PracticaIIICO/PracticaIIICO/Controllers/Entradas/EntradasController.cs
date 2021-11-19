7bfcbc451994d5e40af3c9f0fa913c324703ee4d
Jesús Saenz <jesussaenz04@gmail.com>
2021-11-18 20:13:29 -0600

    Mantenimiento Enttradas

--- PracticaIIICO/PracticaIIICO/Controllers/Entradas/EntradasController.cs
+++ PracticaIIICO/PracticaIIICO/Controllers/Entradas/EntradasController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PracticaIIICO.BD;
 
 namespace PracticaIIICO.Controllers.Entradas
 {
     public class EntradasController : Controller
     {
+        MotoRepuestosMakoEntities ModeloBD = new MotoRepuestosMakoEntities();
+
         // GET: Entradas
         public ActionResult Index()
         {
@@ -15,53 +18,178 @@ namespace PracticaIIICO.Controllers.Entradas
         }
 
         // GET: Entradas/Details/5
-        public ActionResult Details(int id)
+        public ActionResult ListaEntradas()
         {
-            return View();
+            List<sp_Retorna_Entradas_Result> vistaObtenida = new List<sp_Retorna_Entradas_Result>();
+            vistaObtenida = this.ModeloBD.sp_Retorna_Entradas(null).ToList();
+            this.agregaUsuarios();
+
+            return View(vistaObtenida);
         }
 
         // GET: Entradas/Create
-        public ActionResult Create()
+        public ActionResult NuevaEntrada()
         {
+            this.agregaProveedores();
+            this.agregaUsuarios();
+
             return View();
         }
 
         // POST: Entradas/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult NuevaEntrada(sp_Retorna_Entradas_Result modeloVista)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+            DateTime fechaIngreso;
+
+            decimal montoConDescuento;
+            double montoDeIva;
+            decimal montoTotal;
+            double montoBrutoF;
+
+            decimal montoFInalIva;
+
+            montoBrutoF = (double)modeloVista.Monto_Bruto;
+
+            montoConDescuento = modeloVista.Monto_Bruto - modeloVista.Monto_Descuento;
+
+
+            montoDeIva = montoBrutoF * 0.13;
+            montoFInalIva = (Decimal)montoDeIva;
+
+            montoTotal = montoConDescuento + montoFInalIva;
+
+
+            fechaIngreso = DateTime.Now;
+
             try
             {
-                // TODO: Add insert logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Inserta_Entrada(
+                    modeloVista.ID_Proveedor,
+                    modeloVista.ID_Usuario,
+                    modeloVista.Numero_Factura,
+                    modeloVista.Fecha_Factura,
+                    fechaIngreso,
+                    modeloVista.Monto_Bruto,
+                    modeloVista.Monto_Descuento,
+                    montoFInalIva,
+                    montoTotal
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListaEntradas");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            this.agregaProveedores();
+            this.agregaUsuarios();
+
+            return View();
         }
 
         // GET: Entradas/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult ModificaEntrada(int id_Entr)
         {
-            return View();
+            sp_Retorna_EntradaID_Result CollectionObtenido = new sp_Retorna_EntradaID_Result();
+            CollectionObtenido = this.ModeloBD.sp_Retorna_EntradaID(id_Entr).FirstOrDefault();
+
+            this.agregaProveedores();
+            this.agregaUsuarios();
+
+            return View(CollectionObtenido);
         }
 
         // POST: Entradas/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult ModificaEntrada(sp_Retorna_EntradaID_Result modeloVista)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+
+            DateTime fechaIngreso;
+
+            decimal montoConDescuento;
+            double montoDeIva;
+            decimal montoTotal;
+            double montoBrutoF;
+
+            decimal montoFInalIva;
+
+            montoBrutoF = (double)modeloVista.Monto_Bruto;
+
+            montoConDescuento = modeloVista.Monto_Bruto - modeloVista.Monto_Descuento;
+
+
+            montoDeIva = montoBrutoF * 0.13;
+            montoFInalIva = (Decimal)montoDeIva;
+
+            montoTotal = montoConDescuento + montoFInalIva;
+
+
+            fechaIngreso = DateTime.Now;
+
             try
             {
-                // TODO: Add update logic here
+                cantRegistroAfectado = this.ModeloBD.sp_Modifica_Entrada(
+                    modeloVista.ID_Entrada,
+                    modeloVista.ID_Proveedor,
+                    modeloVista.ID_Usuario,
+                    modeloVista.Numero_Factura,
+                    modeloVista.Fecha_Factura,
+                    modeloVista.Fecha_Registro,
+                    modeloVista.Monto_Bruto,
+                    modeloVista.Monto_Descuento,
+                    montoFInalIva,
+                    montoTotal
+                    );
 
-                return RedirectToAction("Index");
+                return RedirectToAction("ListaEntradas");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Modificado";
+                }
+                else
+                {
+                    resultado += "No se pudo Modifcar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            this.agregaProveedores();
+            this.agregaUsuarios();
+
+            return View();
+        }
+        //Get: Entradas/Mostrar
+        public ActionResult MostrarEntrada(int id_Entr)
+        {
+            sp_Retorna_EntradaID_Result collectionObtenido = new sp_Retorna_EntradaID_Result();
+            collectionObtenido = this.ModeloBD.sp_Retorna_EntradaID(id_Entr).FirstOrDefault();
+            this.agregaProveedores();
+            this.agregaUsuarios();
+
+            return View(collectionObtenido);
         }
 
         // GET: Entradas/Delete/5
@@ -84,6 +212,16 @@ namespace PracticaIIICO.Controllers.Entradas
             {
                 return View();
             }
+        }
+
+        void agregaProveedores()
+        {
+            this.ViewBag.ListaProveedores = this.ModeloBD.sp_Retorna_Proveedores(null).ToList();
+        }
+
+        void agregaUsuarios()
+        {
+            this.ViewBag.ListaUsuarios = this.ModeloBD.sp_Retorna_Usuario(null,null).ToList();
         }
     }
 }
