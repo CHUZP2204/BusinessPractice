bd80e2c768ceab8558e32154dd0707f7cdaebcd6
Jesús Saenz <jesussaenz04@gmail.com>
2021-12-07 22:09:20 -0600

    Mantenimiento Citas

--- PracticaIIICO/PracticaIIICO/Controllers/Citas/CitasController.cs
+++ PracticaIIICO/PracticaIIICO/Controllers/Citas/CitasController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PracticaIIICO.BD;
 namespace PracticaIIICO.Controllers.Citas
 {
     public class CitasController : Controller
     {
+        MotoRepuestosMakoEntities ModeloBD = new MotoRepuestosMakoEntities();
+
         // GET: Citas
         public ActionResult Index()
         {
@@ -15,53 +17,146 @@ namespace PracticaIIICO.Controllers.Citas
         }
 
         // GET: Citas/Details/5
-        public ActionResult Details(int id)
+        public ActionResult ListaCitas()
         {
-            return View();
+            List<sp_Retorna_Citas_Result> listaCitas = new List<sp_Retorna_Citas_Result>();
+            listaCitas = this.ModeloBD.sp_Retorna_Citas(null, null).ToList();
+
+
+            this.agregaMarcas();
+            this.agregaUsuarios();
+            return View(listaCitas);
         }
 
         // GET: Citas/Create
-        public ActionResult Create()
+        public ActionResult NuevaCita()
         {
+            this.agregaMarcas();
+            this.agregaUsuarios();
+
+
             return View();
         }
 
         // POST: Citas/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult NuevaCita(sp_Retorna_Citas_Result vistaObtenida)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+            DateTime fechaIngreso;
+            fechaIngreso = DateTime.Now;
+
             try
             {
                 // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
+                cantRegistroAfectado = this.ModeloBD.sp_Inserta_Citas(
+                    vistaObtenida.ID_Usuario,
+                    vistaObtenida.ID_Marca,
+                    vistaObtenida.Nombre_Cliente,
+                    vistaObtenida.Numero_Cliente,
+                    vistaObtenida.Placa_Moto,
+                    vistaObtenida.Fecha_Cita,
+                    vistaObtenida.Hora_Cita,
+                    fechaIngreso
+                    );
+
+                return RedirectToAction("ListaCitas");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
+
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+
+            this.agregaUsuarios();
+            this.agregaMarcas();
+            return View(vistaObtenida);
         }
 
         // GET: Citas/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult ModificaCita(int id_Cita)
         {
-            return View();
+            sp_Retorna_CitasID_Result modeloVista = new sp_Retorna_CitasID_Result();
+            modeloVista = this.ModeloBD.sp_Retorna_CitasID(id_Cita).FirstOrDefault();
+
+            this.agregaUsuarios();
+            this.agregaMarcas();
+
+            return View(modeloVista);
         }
 
         // POST: Citas/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult ModificaCita(sp_Retorna_CitasID_Result vistaObtenida)
         {
+            int cantRegistroAfectado = 0;
+            string resultado = "";
+            DateTime fechaIngreso;
+            fechaIngreso = DateTime.Now;
+
+
             try
             {
                 // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                cantRegistroAfectado = this.ModeloBD.sp_Modifica_Citas(
+                    vistaObtenida.ID_Cita,
+                    vistaObtenida.ID_Usuario,
+                    vistaObtenida.ID_Marca,
+                    vistaObtenida.Nombre_Cliente,
+                    vistaObtenida.Numero_Cliente,
+                    vistaObtenida.Placa_Moto,
+                    vistaObtenida.Fecha_Cita,
+                    vistaObtenida.Hora_Cita,
+                    vistaObtenida.Fecha_Ingreso
+                    );
+                return RedirectToAction("ListaCitas");
             }
-            catch
+            catch (Exception errorObtenido)
             {
-                return View();
+                resultado = "Ocurrio Un Error: " + errorObtenido.Message;
+
             }
+            finally
+            {
+                if (cantRegistroAfectado > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+
+            this.agregaUsuarios();
+            this.agregaMarcas();
+            return View(vistaObtenida);
+        }
+        //
+        public ActionResult MostrarCita(int id_Cita)
+        {
+            sp_Retorna_CitasID_Result datosObtenidos = new sp_Retorna_CitasID_Result();
+            datosObtenidos = this.ModeloBD.sp_Retorna_CitasID(id_Cita).FirstOrDefault();
+
+            this.agregaMarcas();
+            this.agregaUsuarios();
+
+            return View(datosObtenidos);
         }
 
         // GET: Citas/Delete/5
@@ -84,6 +179,15 @@ namespace PracticaIIICO.Controllers.Citas
             {
                 return View();
             }
+        }
+
+        void agregaUsuarios()
+        {
+            this.ViewBag.ListaUsuarios = this.ModeloBD.sp_Retorna_Usuario(null, null).ToList();
+        }
+        void agregaMarcas()
+        {
+            this.ViewBag.ListaMarcas = this.ModeloBD.sp_Retorna_Marcas(null, null).ToList();
         }
     }
 }
